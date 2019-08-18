c3b41c4e00ff3c0fc29ad5efff44a939451aa21f
yuki <yuuki59123@yahoo.co.jp>
2019-08-18 21:20:43 +0900

    make it better

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from datetime import datetime, timedelta
+
+client = discord.Client()
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if member.guild.id == 398875543442948116 and (before.channel != after.channel):
+        now = datetime.utcnow() + timedelta(hours=9)
+        alert_channel = client.get_channel(603979447330406407)
+        if before.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {after.channel.name} に参加しました。'
+            await alert_channel.send(msg)
+        elif after.channel is None:
+            msg = f'{now:%m/%d-%H:%M} に {member.name} が {before.channel.name} から退出しました。'
+            await alert_channel.send(msg)
+
+client.run("NjEyNjA3OTA0NzM4MTgxMTMw.XVk5MA.R86SB_GTdkIS_294qrx-K75FPHk")
